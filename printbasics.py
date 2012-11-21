4ef8b9f66188fd3e47e797308a42251407357b28
algotruneman <algot.runeman@verizon.net>
2012-11-21 14:57:21 -0500

    python print basics

---
+++ printbasics.py
@@ -0,0 +1,27 @@
+########################################
+#
+# printbasics.py
+# Algot Runeman
+# November 21, 2012
+#
+########################################
+
+print 'This is simply a file that prints.'
+print 'It is part of my "Mechanical MOOC" class.'
+print 'This line and the next...',
+print 'print together because of the comma after the ellipsis at the end of "This line".'
+## Using a triple quote """ allows multiline or ascii art stuff.
+print """ONE
+TWO
+THREE"""
+
+print '''
+Happy Tanksgiving!
+____________
+|"""""""""""|=======0
+|____________\____
+|==+==============|
+|__________________\\
+|(@)(@)(@)(@)(@)(@)/
+ *****************
+  '''
